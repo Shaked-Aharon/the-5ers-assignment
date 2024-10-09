495396def7a7dbe6f760a76c8208e2dec89531e9
Shaked Aharon <srek123@gmail.com>
2024-10-09 13:33:41 +0300

    Initial commit

---
+++ apps/the-5ers/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'the-5ers',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/the-5ers',
+};
