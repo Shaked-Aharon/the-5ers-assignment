9f33cf79b4ed4f4c37d7f59aae7533085b1854e2
Shaked Aharon <srek123@gmail.com>
2024-10-10 15:30:35 +0300

    Dont know why but the ConfigModule was missing

--- apps/the-5ers/src/app.module.ts
+++ apps/the-5ers/src/app.module.ts
@@ -4,9 +4,11 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { FinancialsModule } from './financials/financials.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     DatabaseModule,
     AuthModule,
     UsersModule,
